a7c04751d8ef6c3846f5067d11b7574378a1e46c
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-07-02 23:22:33 +0530

    [Ronit]Added : Oops

--- main.py
+++ main.py
@@ -6,13 +6,8 @@ import logging
 
 
 class Comparison:
-    def cartesian_length(self):
+    def cartesian_length(self, x1, x2, y1, y2):
         try:
-            x1 = int(input("enter the x1-coordinate of point1 : "))
-            y1 = int(input("enter the y1-coordinate of point1 : "))
-            x2 = int(input("enter the x2-coordinate of point2 : "))
-            y2 = int(input("enter the y2-coordinate of point2 : "))
-
             length_of_line = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** (1 / 2)
             print("the distance between two point is : ", length_of_line)
             return length_of_line
@@ -20,13 +15,8 @@ class Comparison:
             print(e)
             logging.exception(e)
 
-    def equality_of_length(self):
+    def equality_of_length(self, x3, x4, y3, y4):
         try:
-            x3 = int(input("enter the x2-coordinate of point3 : "))
-            y3 = int(input("enter the y2-coordinate of point3 : "))
-            x4 = int(input("enter the x2-coordinate of point4 : "))
-            y4 = int(input("enter the y2-coordinate of point4 : "))
-
             length_of_line2 = ((x4 - x3) ** 2 + (y4 - y3) ** 2) ** (1 / 2)
             print("the distance between two point is : ", length_of_line2)
             return length_of_line2
@@ -34,22 +24,43 @@ class Comparison:
             print(e)
             logging.exception(e)
 
-    def compare_to(self):
-        try:
-            a = comp_obj.cartesian_length()
-            b = comp_obj.equality_of_length()
-            diff = a - b
-            print("******************************************************************")
-            if diff > 0:
-                print("line-1 is greater!!!")
-            elif diff < 0:
-                print("line-2 is greater!!!")
-            else:
-                print("Both lines are of same length")
-            print("******************************************************************")
-        except Exception as e:
-            print(e)
-            logging.exception(e)
+
+def cartesian_input():
+    comp_obj = Comparison()
+    x1 = int(input("enter the x1-coordinate of point1 : "))
+    y1 = int(input("enter the y1-coordinate of point1 : "))
+    x2 = int(input("enter the x2-coordinate of point2 : "))
+    y2 = int(input("enter the y2-coordinate of point2 : "))
+    a = comp_obj.cartesian_length(x1, x2, y1, y2)
+    return a
+
+
+def equality_input():
+    comp_obj = Comparison()
+    x3 = int(input("enter the x2-coordinate of point3 : "))
+    y3 = int(input("enter the y2-coordinate of point3 : "))
+    x4 = int(input("enter the x2-coordinate of point4 : "))
+    y4 = int(input("enter the y2-coordinate of point4 : "))
+    b = comp_obj.equality_of_length(x3, x4, y3, y4)
+    return b
+
+
+def compare_to():
+    try:
+        a = cartesian_input()
+        b = equality_input()
+        diff = a - b
+        print("******************************************************************")
+        if diff > 0:
+            print("line-1 is greater!!!")
+        elif diff < 0:
+            print("line-2 is greater!!!")
+        else:
+            print("Both lines are of same length")
+        print("******************************************************************")
+    except Exception as e:
+        print(e)
+        logging.exception(e)
 
 
 if __name__ == '__main__':
@@ -63,9 +74,7 @@ if __name__ == '__main__':
     logging.debug("Line comparison computation Program running................")
 
     try:
-        comp_obj = Comparison()
-        result = comp_obj.compare_to()
-        print(result)
+        compare_to()
     except Exception as e:
         print(e)
         logging.exception(e)
